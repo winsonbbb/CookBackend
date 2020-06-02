4fa0bd0505e872067b1ac8c60228a2ee6b494d43
Ricky Liu <ricky.liu@arch-partnership.com>
2020-06-02 17:52:18 +0800

    init

---
+++ src/main/java/mummy/cook/easyCook/EasyCookApplication.java
@@ -0,0 +1,13 @@
+package mummy.cook.easyCook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyCookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyCookApplication.class, args);
+	}
+
+}
